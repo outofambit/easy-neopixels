0294850053c83b5dd2be18526afaaf972ffc7a06
Evelyn Masso <outofambit@github.com>
2020-01-12 18:05:44 -0800

    update example comments

--- examples/blink/blink.ino
+++ examples/blink/blink.ino
@@ -1,18 +1,21 @@
 /*
 
-Blink a single Neopixel!
+Blink bright white light from a single Neopixel!
 
 */
 
 #include <EasyNeoPixels.h>
 
 void setup() {
+    // setup for one NeoPixel attached to pin 13
     setupEasyNeoPixels(13, 1);
 }
 
 void loop() {
+    // turn the NeoPixel ON
     writeEasyNeoPixel(0, HIGH);
     delay(500);
+    // turn the NeoPixel OFF
     writeEasyNeoPixel(0, LOW);
     delay(500);
 }
